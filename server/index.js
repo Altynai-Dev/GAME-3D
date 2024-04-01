0c6f4a0a4c6db0590723cc921b2259cee231e76c
Altynai-Dev <altynai.imnl@gmail.com>
2024-04-01 15:57:15 -0400

    added headers

--- server/index.js
+++ server/index.js
@@ -17,6 +17,13 @@ const io = new Server({
   },
 });
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 const characters = [];
 
 const items = {
